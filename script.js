75bc99aaac08af2fc83eb111d7d25dfc1ab036d3
Manasa Golla <msgolla04@gmail.com>
2025-06-20 13:43:48 -0400

    changes

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function toggleMenu() {
     icon.classList.toggle("open")
 
 }
+
+
+//TO DO
+/*
+1. Figure out how to make experience look better. how to better show this? sepearte into sections? maybe once i get more work experience idk
+    also add images to the expeirence if possible
+2. ADD EDUCATION TO ABOUT SECTION
+3. personal section: first need to make each boc so it opens up to a new page
+    - Travel: include the video i made from my spam. have a list of diffrent places i have been to (omg interactice map)
+    - Food: Link my beli AND my instagram food account
+    - Reads: how can i develop a fun bookshelf find of page
+4. Need to add links to different things (linked in, email, github, etc)
+    4
+
+*/
