a9c9b0f0da6310a58db368d6e4eefe7dc9c60599
Manasa Golla <msgolla04@gmail.com>
2025-06-23 15:51:12 -0400

    temp updates

--- script.js
+++ script.js
@@ -5,15 +5,48 @@
 /*
 1. Figure out how to make experience look better. how to better show this? sepearte into sections? maybe once i get more work experience idk
     also add images to the expeirence if possible
-2. ADD EDUCATION TO ABOUT SECTION
-3. personal section: first need to make each boc so it opens up to a new page
+2. personal section: 
     - Travel: include the video i made from my spam. have a list of diffrent places i have been to (omg interactice map)
     - Food: Link my beli AND my instagram food account
     - Reads: how can i develop a fun bookshelf find of page
-4. Need to add links to different things (linked in, email, github, etc)
-5. i want to change fonts/themes to make it more personal and more like me
-6. for the personal section, there is a weird gap in betwen the sections!!
+3. i want to change fonts/themes to make it more personal and more like me
+4. for the personal section, there is a weird gap in betwen the sections!!
+5. maybe make buttons in center?
+
+Ideas:
+experience - make it so it's intuitive to click on each one and when it's clicked ,THEN the bullet point sshow up+ the extra infromation!!  
+i  also want a space to add the logo for the images!!
+
+FIGURE OUT how to put te buttends (email, github,linkedin) in van gar where it is clean and makes sense
+
+i kind of what to list projects a bit differentely as well. like more CLEAN and miminal. 
+similar to experience section where if u click on the square, it will expand the block into what it currently looks like. (immage+title -click-> image+tite+descriptions+techlinks)
 
 
-
+BUT I DONT KNOW!!
+Goal: make it look cleaner, a bit unique, but intuite
+for the personal section, there is a weird gap in betwen the sections!!
 */
+
+
+
+
+/* 
+    <!-- Footer -->
+<!--     <footer class="footer">
+        <div class="footer-content">
+            <div class="social-links">
+                <a href="#" class="social-link">📧</a>
+                <a href="#" class="social-link">💼</a>
+                <a href="#" class="social-link">🐙</a>
+                <a href="#" class="social-link">🐦</a>
+            </div>
+            <p class="footer-text">
+                © 2025 Your Name. Built with passion and lots of coffee ☕
+            </p>
+        </div>
+    </footer>
+ -->
+    
+ */
+    
